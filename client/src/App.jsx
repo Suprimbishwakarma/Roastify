4801172f32347747246aae6aea3b49a229633660
Suprim bishwakarma <suprimbishwakarma2002@gmail.com>
2025-12-31 15:44:25 +0545

    react-router configuration

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Landing from "./pages/Landing/Landing";
+import Dashboard from "./pages/Dashboard/Dashboard";
+import RoastDisplay from "./components/RoastDisplay/RoastDisplay";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Landing />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/roast" element={<RoastDisplay />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
