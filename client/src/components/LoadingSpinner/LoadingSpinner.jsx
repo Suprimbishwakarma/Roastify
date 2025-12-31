96a7110f1de289bc6e6d273424db164596700120
Suprim bishwakarma <suprimbishwakarma2002@gmail.com>
2025-12-31 20:10:41 +0545

    simple loading spinner

--- client/src/components/LoadingSpinner/LoadingSpinner.jsx
+++ client/src/components/LoadingSpinner/LoadingSpinner.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import "./LoadingSpinner.css";
+import { LuLoaderPinwheel } from "react-icons/lu";
 
-const LoadingSpinner = () => {
-  return <div>LoadingSpinner</div>;
+const LoadingSpinner = ({ message = "Loading..." }) => {
+  return (
+    <div className="loading">
+      <LuLoaderPinwheel className="loader" />
+      <p className="loading-message">{message}</p>
+    </div>
+  );
 };
 
 export default LoadingSpinner;
