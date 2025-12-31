e7643049c960aa5095a051ecc051e427497100a3
Suprim bishwakarma <suprimbishwakarma2002@gmail.com>
2025-12-31 09:37:58 +0545

    frontend initial boilerplate

---
+++ client/src/components/MusicStats/MusicStats.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./MusicStats.css";
+
+const MusicStats = () => {
+  return <div>MusicStats</div>;
+};
+
+export default MusicStats;
---
+++ client/src/components/RoastDisplay/RoastDisplay.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./RoastDisplay.css";
+
+const RoastDisplay = () => {
+  return <div>RoastDisplay</div>;
+};
+
+export default RoastDisplay;
