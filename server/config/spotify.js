1893e1ec36b8a8c196ca6988199e1f4d2bf5ffed
suprimbishwakarma <suprimbishwakarma2002@gmail.com>
2026-01-08 11:51:19 +0545

    Deployment issue fixes

--- server/config/spotify.js
+++ server/config/spotify.js
@@ -1,9 +1,9 @@
 import SpotifyWebApi from "spotify-web-api-node";
 
 const spotifyAPI = new SpotifyWebApi({
-  clientId: process.env.CLIENT_ID,
-  clientSecret: process.env.CLIENT_SECRET,
-  redirectUri: process.env.REDIRECT_URI,
+  clientId: process.env.SPOTIFY_CLIENT_ID,
+  clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+  redirectUri: process.env.SPOTIFY_REDIRECT_URI,
 });
 
 export default spotifyAPI;
